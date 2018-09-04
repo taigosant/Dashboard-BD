0ce1d73f03faf894dc9ff6895f5b15c82fa21964
Katiely <katiely.eu@gmail.com>
2018-09-04 17:55:45 -0400

    [INS] inserting class review without cursors

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from models.Product import Product
+from models.Review import Review
 from Parser import Parser
 from ManagerDB import Manager
 path = 'tokens.pkl'
 
 if __name__ == '__main__':
-    print('loading data...') #Todo talvez contabilizar tempo de loading e percentual, isso é só frescura pra caso não tenha mais nada pra fazer
-    parser = Parser()
-    parser.parse(path)
-    mapProd = parser.getProductsMap()
-    print('Products length: ', len(mapProd))
-    # print(mapProd[693].toString())
-    manager = Manager('bdzinho', 'dashboard', 'rorschach')
-    manager.connect()
-    if manager.isConnected():
-        manager.bunkInsert(mapProd)
+    p =  Review(10, "20-10-2008", 3, 4, 10,15,"4834")
+    print(p.toString())
+    # print('loading data...') #Todo talvez contabilizar tempo de loading e percentual, isso é só frescura pra caso não tenha mais nada pra fazer
+    # parser = Parser()
+    # parser.parse(path)
+    # mapProd = parser.getProductsMap()
+    # print('Products length: ', len(mapProd))
+    # # print(mapProd[693].toString())
+    # manager = Manager('bdzinho', 'dashboard', 'rorschach')
+    # manager.connect()
+    # if manager.isConnected():
+    #     manager.bunkInsert(mapProd)
 
 
 
