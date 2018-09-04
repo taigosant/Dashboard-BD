1a79a78959bec76cc33998c340ed378d84c4a71b
Katiely <katiely.eu@gmail.com>
2018-09-04 18:08:36 -0400

    [INS] inserting class Category without cursors
    So criando a classe bozo de categoria sem acesso a banco , so criando o objeto msm

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ from ManagerDB import Manager
 path = 'tokens.pkl'
 
 if __name__ == '__main__':
-    p =  Review(10, "20-10-2008", 3, 4, 10,15,"4834")
-    print(p.toString())
-    # print('loading data...') #Todo talvez contabilizar tempo de loading e percentual, isso é só frescura pra caso não tenha mais nada pra fazer
-    # parser = Parser()
-    # parser.parse(path)
-    # mapProd = parser.getProductsMap()
-    # print('Products length: ', len(mapProd))
-    # # print(mapProd[693].toString())
-    # manager = Manager('bdzinho', 'dashboard', 'rorschach')
-    # manager.connect()
-    # if manager.isConnected():
-    #     manager.bunkInsert(mapProd)
+    print('loading data...') #Todo talvez contabilizar tempo de loading e percentual, isso é só frescura pra caso não tenha mais nada pra fazer
+    parser = Parser()
+    parser.parse(path)
+    mapProd = parser.getProductsMap()
+    print('Products length: ', len(mapProd))
+    # print(mapProd[693].toString())
+    manager = Manager('bdzinho', 'dashboard', 'rorschach')
+    manager.connect()
+    if manager.isConnected():
+        manager.bunkInsert(mapProd)
 
 
 
