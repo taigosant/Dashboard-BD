6c7b9edbb30f8efd311126510b33d89d8cd8ad07
Katielys <katiely.eu@gmail.com>
2018-09-05 20:36:37 -0400

    [INS] review without cursor and parser getting review onebyone

--- Parser.py
+++ Parser.py
@@ -3,9 +3,8 @@ import pickle
 import re
 from models.Product import Product
 from models.Category import Category
-
-# path = '/home/taigo/Documents/2018.2/BD/Dashboard-BD/data/amazon-meta.txt'
-path = '/home/taigo/Documents/2018.2/BD/Dashboard-BD/tokens.pkl'
+from models.Review import Review
+path = 'tokens.pkl'
 
 #regexes
 ASIN = r"ASIN: (.*)"
@@ -13,13 +12,13 @@ TITLE = r"title: (.*)"
 GROUP = r"group: (\w*)"
 SALESRANK = r"salesrank: (\d*)"
 CATEGORY_CHUNK = r"categories:((.|\n)*)reviews"
-# CATEGORY_CHUNK = r"categories: \d+\n(\|(\w| | \W)*\[\d*\]|\n|)*s"
 
 
 class Parser(object):
     def __init__(self):
         self.__mapProduct = {}
         self.__mapCategorioes = {}
+        self.__mapReview = {}
 
     def __extractCategories(self, stringList):
         for string in stringList:
@@ -38,22 +37,72 @@ class Parser(object):
 
                 except Exception as e:
                     print(e)
-
-
-
-
+    def __extractReview(self,stringList,id):
+        helpful = None
+        costumer = None
+        date = None
+        rating = None
+        votes = None
+        aux = None
+        if(len(stringList) > 1 ):
+            stringList = stringList[1:]
+            
+        for string in stringList:
+            
+            print(string)
+            
+            date = string.find("cutomer")
+            if (date != -1):
+                date = string[4:date-1]
+                
+            costumer = string.find("cutomer: ")
+            aux =  string.find("  rating:")
+            if(costumer!=-1):
+                costumer =  string[costumer+9:aux]
+            rating = string.find("rating: ")   
+            aux = string.find("  votes")
+            if(rating!=-1):
+                rating = string[rating+8:aux]
+            votes   = string.find("votes:   ") 
+            aux = string.find("  helpful")
+            if(votes!=-1):
+                votes =  string[votes+9:aux]
+            aux =  string.find("helpful:   ") 
+            if(aux!=-1):
+                helpful =  string[aux+11:len(string)]
+            r = Review(date,votes,rating,helpful,id,costumer)
+            print(r.toString())
+            self.__mapReview[id] = r
+            
+                
+                
+                
+                
+             
+            
+                   
 
     def parse(self, path):
         with open(path, 'rb') as pFile:
             tokens = pickle.load(pFile)
             id = 0
             for token in tokens:
-                # print(token)
+                #print(token)
                 asins = re.findall(ASIN, token, re.MULTILINE)
                 titles = re.findall(TITLE, token, re.MULTILINE)
                 groups = re.findall(GROUP, token, re.MULTILINE)
                 salesranks = re.findall(SALESRANK, token, re.MULTILINE)
                 categoryChunk = re.findall(CATEGORY_CHUNK, token, re.MULTILINE)
+                
+                reviewChunk =  token.split("reviews:")
+                if (len(reviewChunk)>1):
+                    try:
+                        review =  reviewChunk[1].split("\n")                
+                        #print("REVIEW ",(review))
+                        self.__extractReview(review,id)
+                    except Exception as e:
+                        print(e, '\n', review)    
+                     
                 categories = []
                 if len(categoryChunk) > 0:
                     try:
@@ -63,8 +112,7 @@ class Parser(object):
                         self.__extractCategories(categories)
                     except Exception as e:
                         print(e, '\n', text)
-
-
+               
                 print('ID:', id)
                 print('ASIN:', asins)
                 print('TITLE:', titles)
